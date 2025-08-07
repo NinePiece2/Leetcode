6b18ed4feeff6e72990aacf0795ec3c2b2303c77
Romit Sagu <romit.sagu@hotmail.com>
2025-08-07 05:50:09 +0000

    [LeetCode Sync] Runtime - 2 ms (33.97%), Memory - 17.9 MB (34.29%)

---
+++ Solutions/0020-valid-parentheses/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        maps = { ')': '(', '}':'{', ']':'['}
+
+        for char in s:
+            if char in maps:
+                top = stack.pop() if stack else ''
+                if maps[char] != top:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
