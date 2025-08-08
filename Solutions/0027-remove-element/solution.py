4ecdede11ba99f54d3949721f0e46a4d174efd8b
Romit Sagu <romit.sagu@hotmail.com>
2025-08-08 19:28:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (83.33%)

--- Solutions/0027-remove-element/solution.py
+++ Solutions/0027-remove-element/solution.py
@@ -1,8 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        for i in range (len(nums)):
-            if (nums[i + k] != val):
-                nums.insert(0, nums[i + k])
-                k += 1
-        return k
+        not_removed = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[not_removed] = nums[i]
+                not_removed += 1
+
+        return not_removed
+
