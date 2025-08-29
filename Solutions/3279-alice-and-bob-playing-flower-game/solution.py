74ce8e6ff2632ca7d41d17377fc5a1b79673bc2b
Romit Sagu <romit.sagu@hotmail.com>
2025-08-29 03:18:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (51.12%)

---
+++ Solutions/3279-alice-and-bob-playing-flower-game/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        a1 = (n + 1) // 2
+        b1 = (m + 1) // 2
+
+        a2 = n // 2
+        b2 = m // 2
+
+        return (a1 * b2) + (b1 * a2)
+        #return (n * m)// 2
