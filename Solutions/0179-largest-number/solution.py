5ae4aa9d88b51034e09a735afbba0d0ca3b48a88
Romit Sagu <romit.sagu@hotmail.com>
2025-09-01 23:25:54 +0000

    [LeetCode Sync] Runtime - 3 ms (52.94%), Memory - 17.7 MB (86.23%)

--- Solutions/0179-largest-number/solution.py
+++ Solutions/0179-largest-number/solution.py
@@ -1,15 +1,5 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        nums_str = [str(num) for num in nums]
-    
-        nums_str_len = len(nums_str)
-        for i in range(nums_str_len):
-            for j in range(0, nums_str_len - i - 1):
-                if nums_str[j] + nums_str[j + 1] < nums_str[j + 1] + nums_str[j]:
-                    # Swap if they are in the wrong order
-                    nums_str[j], nums_str[j + 1] = nums_str[j + 1], nums_str[j]
-        
-        result = ''.join(nums_str)
-        
-        return '0' if result[0] == '0' else result
-
+        vals = [str(val) for val in nums]
+        vals.sort(key=cmp_to_key(lambda a, b: 1 if a + b < b + a else -1))
+        return "0" if vals[0] == "0" else "".join(vals)
