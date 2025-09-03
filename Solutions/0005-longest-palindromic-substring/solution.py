bb7b960618f0caaf065be4a30ed29759ecbc68d1
Romit Sagu <romit.sagu@hotmail.com>
2025-09-03 05:53:39 +0000

    [LeetCode Sync] Runtime - 1811 ms (27.26%), Memory - 25.5 MB (11.80%)

--- Solutions/0005-longest-palindromic-substring/solution.py
+++ Solutions/0005-longest-palindromic-substring/solution.py
@@ -1,25 +1,15 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        # Edge cases
-        if not s or len(s) <= 1:
-            return s
-        
-        start, end = 0, 0
+        n = len(s)
+        dp = [[True] * n for _ in range(n)]
+        k, max_num = 0, 1
 
-        for i in range(len(s)):
-            len1 = self.expandFromCenter(s, i, i)
-            len2 = self.expandFromCenter(s, i, i + 1)
-            max_len = max(len1, len2)
-
-            if max_len > (end - start):
-                start = i - (max_len - 1) // 2
-                end = i + max_len // 2
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                dp[i][j] = False
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1]
+                    if dp[i][j] and max_num < j - i + 1:
+                        k, max_num = i, j - i + 1
         
-        return s[start:end+1]
-    
-    def expandFromCenter(self, s, left, right):
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        return right - left - 1
+        return s[k : k + max_num]
