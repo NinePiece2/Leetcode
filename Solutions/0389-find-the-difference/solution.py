d8ffafaccba9d5464513c5f73e223e9a01a0b559
Romit Sagu <romit.sagu@hotmail.com>
2025-09-25 18:15:33 +0000

    [LeetCode Sync] Runtime - 3 ms (51.44%), Memory - 18 MB (25.11%)

---
+++ Solutions/0389-find-the-difference/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        count = Counter(s)
+        for char in t:
+            count[char] -= 1
+            if count[char] < 0:
+                return char
+
