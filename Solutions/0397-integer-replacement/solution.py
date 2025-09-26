b1a22b432d63ca847817d0f1e0c78afb34bf822e
Romit Sagu <romit.sagu@hotmail.com>
2025-09-26 06:21:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (74.42%)

---
+++ Solutions/0397-integer-replacement/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        result = 0
+        while n != 1:
+            if (n & 1) == 0:
+                n >>= 1
+            elif n != 3 and (n & 3) == 3:
+                n += 1
+            else:
+                n-= 1
+            result += 1
+        return result
