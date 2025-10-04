4b8f85943cc260101bb535ffce057fbdb20d99b1
Romit Sagu <romit.sagu@hotmail.com>
2025-10-04 00:42:02 +0000

    [LeetCode Sync] Runtime - 108 ms (29.23%), Memory - 28.5 MB (23.74%)

--- Solutions/0011-container-with-most-water/solution.py
+++ Solutions/0011-container-with-most-water/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # two pointer solution
         left, right = 0, len(height) - 1
         max_area = 0
 
-        while left < right:
-            h = min(height[left], height[right])
-            w = right - left
-            max_area = max(max_area, h * w)
+        while right > left:
+            height_cantidate = min(height[right], height[left])
+            width = right - left
+            max_area = max(max_area, height_cantidate * width)
 
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
+            
         return max_area
