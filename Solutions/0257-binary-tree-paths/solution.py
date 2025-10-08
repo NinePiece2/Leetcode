8dd7ff0359ebcb87bd1b51c61b11d0efa0839b8e
Romit Sagu <romit.sagu@hotmail.com>
2025-10-08 04:10:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (44.24%)

---
+++ Solutions/0257-binary-tree-paths/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        result = []
+        temp = []
+        def dfs(root: Optional[TreeNode]):
+            if root is None:
+                return
+            temp.append(str(root.val))
+            if root.left is None and root.right is None:
+                result.append("->".join(temp))
+            else:
+                dfs(root.left)
+                dfs(root.right)
+            temp.pop()
+
+        dfs(root)
+        return result
