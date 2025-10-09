5f6fcd71d03494d972321efcdfbeef35da3da781
Romit Sagu <romit.sagu@hotmail.com>
2025-10-09 00:22:30 +0000

    [LeetCode Sync] Runtime - 3 ms (86.58%), Memory - 21.7 MB (58.30%)

---
+++ Solutions/0130-surrounded-regions/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m, n = len(board), len(board[0])
+        def dfs(i: int, j: int):
+            if not (0 <= i < m and 0 <= j < n and board[i][j] == "O"):
+                return
+            board[i][j] = "#"
+            for dir1, dir2 in pairwise((-1, 0, 1, 0, -1)):
+                dfs(i + dir1, j + dir2)
+
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n - 1)
+
+        for j in range(n):
+            dfs(0, j)
+            dfs(m - 1, j)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "#":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
