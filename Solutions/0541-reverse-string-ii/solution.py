073fed152900b65feb014077ca724f0173183165
Romit Sagu <romit.sagu@hotmail.com>
2025-10-10 20:49:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.1 MB (8.01%)

---
+++ Solutions/0541-reverse-string-ii/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        chars = list(s)
+        for i in range(0, len(s), k * 2):
+            chars[i : i + k] = reversed(s[i : i + k])
+        return "".join(chars)
