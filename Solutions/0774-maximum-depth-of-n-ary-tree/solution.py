1e1ebdfe3b202f06c045a0cdd84655c0b93e792c
Romit Sagu <romit.sagu@hotmail.com>
2025-10-10 21:16:55 +0000

    [LeetCode Sync] Runtime - 46 ms (60.35%), Memory - 19.1 MB (66.71%)

---
+++ Solutions/0774-maximum-depth-of-n-ary-tree/solution.py
@@ -0,0 +1,16 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if root is None:
+            return 0
+        result = 0
+        for child in root.children:
+            result = max(result, self.maxDepth(child))
+        return result + 1
