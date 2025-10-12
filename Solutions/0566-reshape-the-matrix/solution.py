11091686b9293a2db48d119518c7393f8c36f2c2
Romit Sagu <romit.sagu@hotmail.com>
2025-10-12 18:13:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.6 MB (33.31%)

---
+++ Solutions/0566-reshape-the-matrix/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        if m * n != r * c:
+            return mat
+
+        result = [[0] * c for _ in range(r)]
+        for i in range(m * n):
+            result[i // c][i % c] = mat[i // n][i % n]
+        return result
