571fec97e15aea8476f2e6dd5367dd2f260e7d78
Romit Sagu <romit.sagu@hotmail.com>
2025-10-18 06:58:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (62.94%)

---
+++ Solutions/0168-excel-sheet-column-title/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        result = []
+        while columnNumber:
+            columnNumber -= 1
+            result.append(chr(ord("A") + columnNumber % 26))
+            columnNumber //= 26
+        
+        return "".join(result[::-1])
