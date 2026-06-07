0e099239f979a556ed8efc45ce40b6a22498626d
Romit Sagu <romit.sagu@hotmail.com>
2026-06-07 01:57:21 +0000

    [LeetCode Sync] Runtime - 15 ms (98.36%), Memory - 28.4 MB (96.14%)

--- Solutions/0121-best-time-to-buy-and-sell-stock/solution.py
+++ Solutions/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -4,7 +4,12 @@ class Solution:
         best = 0
 
         for day_price in prices:
-            min_price = min(min_price, day_price)
-            best = max(best, day_price - min_price)
+            if min_price < day_price:
+                profit = day_price - min_price
+                if profit > best:
+                    best = profit
+                    
+            else:
+                min_price = day_price
         
         return best
