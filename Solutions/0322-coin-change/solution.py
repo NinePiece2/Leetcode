102c4a6f6acd1307fc8730cc54f8c71c31eddee7
Romit Sagu <romit.sagu@hotmail.com>
2026-06-07 02:29:57 +0000

    [LeetCode Sync] Runtime - 427 ms (95.40%), Memory - 19.5 MB (77.62%)

--- Solutions/0322-coin-change/solution.py
+++ Solutions/0322-coin-change/solution.py
@@ -1,13 +1,25 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        m, n = len(coins), amount
-        dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = 0
+        # m, n = len(coins), amount
+        # dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
+        # dp[0][0] = 0
 
-        for i, coin in enumerate(coins, 1):
-            for j in range(n + 1):
-                dp[i][j] = dp[i - 1][j]
-                if j >= coin:
-                    dp[i][j] = min(dp[i][j], dp[i][j - coin] + 1)
+        # for i, coin in enumerate(coins, 1):
+        #     for j in range(n + 1):
+        #         dp[i][j] = dp[i - 1][j]
+        #         if j >= coin:
+        #             dp[i][j] = min(dp[i][j], dp[i][j - coin] + 1)
 
-        return -1 if dp[-1][-1] == float('inf') else dp[-1][-1]
+        # return -1 if dp[-1][-1] == float('inf') else dp[-1][-1]
+
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        if dp[amount] == amount + 1:
+            return -1
+        else:
+            return dp[-1]
